23f68162c936d69f4bce1af30d5854ad5acfb3da
Max <maxdevsmart0408@gmail.com>
2021-10-04 12:50:25 +0300

    feat: add perch swap sdk

---
+++ dist/constants.d.ts
@@ -0,0 +1,38 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    BSCTESTNET = 97,
+    POLYGON = 137,
+    AVAX = 43114,
+    AVAXTESTNET = 43113
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x014b335DC90e3EE45a67334E7436C71F419D1F1A";
+export declare const INIT_CODE_HASH = "0x3032aa4d3196f9ad0690d7d922ba894352c56813e8f492fdfb9286ad7c4a66a6";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const _998: JSBI;
+export declare const _1000: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
